39242e3371d6d47c1f43866e10a5e08540301759
JackCarron <john.jack.carron@gmail.com>
2021-02-14 16:49:25 -0700

    Stable version for text & html

--- api/controller.py
+++ api/controller.py
@@ -4,6 +4,7 @@ from gmail_service import get_most_recent_aws_sns_email
 import json
 from flask import jsonify
 import re
+import quopri
 
 
 app = flask.Flask(__name__)
@@ -15,14 +16,24 @@ def home():
     payload = get_most_recent_aws_sns_email()
     # payload = payload[:-2] + '"}'
     #
-    # regex = re.compile(r'\\(?![/u"])')
-    # fixed = regex.sub(r"\\\\", payload)
-    # regex = re.compile(r'--(?s)(.*)support')
+    #regex = re.compile(r'--(?s)(.*)support')
     # aws_payload = regex.sub(r'',json.loads(fixed)['content'])
     # print(type(aws_payload))
     # aws_payload = jsonify(aws_payload.split(';\\\\'))
-    print(type(payload[0]))
-    return json.loads(payload[0])
+    #print(type(payload[0]))
+    curr_message = payload[1]
+    quotable_printable = json.loads(json.loads(curr_message)['Message'])['content']. \
+      split('Content-Transfer-Encoding: quoted-printable')
+    text_plain = json.loads(json.loads(curr_message)['Message'])['content']. \
+      split('Content-Type: text/plain')
+    if len(quotable_printable) > 1:
+        print(payload)
+        decoded_string = quopri.decodestring(payload)
+        print(decoded_string.decode('utf-8'))
+        return decoded_string
+    elif len(text_plain) > 1:
+        return text_plain[1]
+    return jsonify(quotable_printable)
     #file = open('output/html_index_7.html','r')
     #removed_html_tags = file.read().replace('<html>','').replace('</html>','')
     #print(removed_html_tags)
