57b28e2cf6b53df3833fed5f2e36396fc6a8364a
JackCarron <john.jack.carron@gmail.com>
2021-02-13 19:52:54 -0700

    Stable version of writing files

--- gmail1.py
+++ gmail1.py
@@ -6,10 +6,12 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 import json
 import base64
+import webbrowser
 
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
-NUM_OF_MESSAGES = 20
+BASE_URL = 'file:///Library/Frameworks/Python.framework/Versions/3.8/bin/scripts/GmailReader/'
+NUM_OF_MESSAGES = 25
 
 
 def main():
@@ -25,32 +27,43 @@ def main():
 
 def message_parser(messages, num_of_messages, service):
     for i in range(num_of_messages):
-        is_text_flag = False
-        # curr_message_info is main object
         curr_message_info = service.users().messages() \
-        .get(userId='me',id=messages[i]['id']).execute()
+            .get(userId='me',id=messages[i]['id']).execute()
         headers = curr_message_info['payload']['headers']
         for j in range(len(headers)):
-            print('index #: ' + str(j))
+            file_name = ''
             if headers[j]['name'] != None and headers[j]['name'] == 'Content-Type':
-                print(json.dumps(headers[j],indent=2))
                 if 'multipart/alternative' in headers[j]['value']:
-                    is_text_flag = True
-        if is_text_flag:
-            multipart_alt_parser(curr_message_info)
-        print('-------------------------------')
+                    multipart_text = multipart_alt_parser(curr_message_info['payload']['parts'][0])
+                    file_name = 'output/multipart_text_index_' + str(i) + '.txt'
+                    write_file_utility(file_name, multipart_text)
+                
+                elif 'text/html' in headers[j]['value']:
+                    decoded_html = decode_string(curr_message_info['payload']['body']['data'])
+                    file_name = 'output/html_index_' + str(i) + '.html'
+                    write_file_utility(file_name, decoded_html)
+
+                webbrowser.open(BASE_URL + file_name,new=2)
+                continue
         print('-------------' + str(i) + '----------------')
-        print('-------------------------------')
         
 
 def multipart_alt_parser(message):
-        message_info = message['payload']['parts'][0]
-        message = message_info['body']['data']
-        decoded_message = base64.urlsafe_b64decode(message)
-        decoded_message_list = str(decoded_message).split('\\r\\n')
-        for m in decoded_message_list:
-            print(m)
+        message_info = message
+        message = message['body']['data']
+        return decode_string(message)
 
+
+
+def decode_string(message):
+    decoded_message = base64.urlsafe_b64decode(message)
+    return str(decoded_message).replace('\\r','').replace('\\t','').replace('\\n','')
+    
+
+def write_file_utility(file_name,file_content):
+    file= open(file_name,'w')
+    file.write(file_content)
+    file.close()
 
 
 def getCreds():
